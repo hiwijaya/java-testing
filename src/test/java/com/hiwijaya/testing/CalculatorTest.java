a23338b8e949242d0a7ed9b72948f61aee4a0afd
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2021-02-15 16:06:17 +0700

    assertEquals

--- src/test/java/com/hiwijaya/testing/CalculatorTest.java
+++ src/test/java/com/hiwijaya/testing/CalculatorTest.java
@@ -1,48 +1,20 @@
 package com.hiwijaya.testing;
 
-import org.junit.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Happy Indra Wijaya
  */
 public class CalculatorTest {
 
-    @BeforeClass
-    public static void initGlobal(){
-        System.out.println("Initiate for global usage.");
-    }
-
-    @AfterClass
-    public static void cleanupGlobal(){
-        System.out.println("Reset the global usage.");
-    }
-
-    @Before
-    public void initTest(){
-        System.out.println("Initiate for test unit");
-    }
-
-    @After
-    public void cleanupTest(){
-        System.out.println("Reset the test unit");
-    }
+    private Calculator calculator = new Calculator();
 
     @Test
-    public void testAdd(){
-        Calculator calculator = new Calculator();
-        int result = calculator.add(10, 5);
-        System.out.println("10 + 5 = " + result);
-
-        Assert.assertEquals("10 + 5 = 15", 15, calculator.add(10, 5));
+    public void testAddSuccess(){
+        var result = calculator.add(5, 10);
+        assertEquals(15, result);
     }
 
-    @Test
-    public void testSubtract(){
-        Calculator calculator = new Calculator();
-        int result = calculator.subtract(10, 15);
-        System.out.println("10 - 15 = " + result);
-
-        Assert.assertEquals("10 - 15 = -5", -5, calculator.subtract(10, 15));
-    }
 
 }
