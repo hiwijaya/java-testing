cd3388a06e147eb5b808ce830781b2517aee796b
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2021-02-15 17:30:11 +0700

    added @DisplayName

--- src/test/java/com/hiwijaya/testing/CalculatorTest.java
+++ src/test/java/com/hiwijaya/testing/CalculatorTest.java
@@ -1,28 +1,34 @@
 package com.hiwijaya.testing;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Happy Indra Wijaya
  */
+@DisplayName("Run test on Calculator class")
 public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
 
     @Test
+    @DisplayName("Test add(), expected ok")
     public void testAddSuccess(){
         var result = calculator.add(5, 10);
         assertEquals(15, result);
     }
 
     @Test
+    @DisplayName("Test divide(), expected ok")
     public void testDivideSuccess(){
         var result = calculator.divide(10, 2);
         assertEquals(5, result);
     }
 
     @Test
+    @DisplayName("Test method divide(), expected thrown expected thrown exception")
     public void testDivideFailed(){
         assertThrows(IllegalArgumentException.class, () -> {
             calculator.divide(10, 0);
