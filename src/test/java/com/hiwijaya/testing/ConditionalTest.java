237857cc74e4a1e469955b25928004272b9c2002
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2021-02-16 14:54:40 +0700

    conditional test

---
+++ src/test/java/com/hiwijaya/testing/ConditionalTest.java
@@ -0,0 +1,49 @@
+package com.hiwijaya.testing;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.condition.OS;
+import org.opentest4j.TestAbortedException;
+
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+/**
+ * @author Happy Indra Wijaya
+ */
+public class ConditionalTest {
+
+    @BeforeAll
+    public static void setup(){
+        Properties properties = System.getProperties();
+        properties.forEach((key, value) -> System.out.println(key + ": " + value));
+    }
+
+    @Test
+    public void testAborted(){
+        var profile = System.getenv("PROFILE");
+        if(!"DEV".equals(profile)){
+            throw new TestAbortedException("Aborted. Test only run on DEV environment.");
+        }
+    }
+
+    @Test
+    public void testAssumptions(){
+        assumeTrue("DEV".equals(System.getenv("PROFILE")));
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "PROFILE", matches = "DEV")
+    public void testEnabledOnProfileDev(){
+        System.out.println("Test enabled when profile is DEV");
+    }
+
+    @Test
+    @EnabledOnOs({OS.WINDOWS})
+    public void testEnabledOnWindowsOS(){
+        System.out.println("Test enabled when OS is Windows");
+    }
+
+}
