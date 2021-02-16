3f59151659397a4767b1ee9da263b66b6da03669
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2021-02-16 11:55:49 +0700

    before and after unit test

--- src/test/java/com/hiwijaya/testing/CalculatorTest.java
+++ src/test/java/com/hiwijaya/testing/CalculatorTest.java
@@ -1,38 +1,55 @@
 package com.hiwijaya.testing;
 
-import com.hiwijaya.testing.util.CustomDisplayNameGenerator;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Happy Indra Wijaya
  */
-//@DisplayName("Run test on Calculator class")
-//@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
-@DisplayNameGeneration(CustomDisplayNameGenerator.class)
+@DisplayName("Run test on Calculator class")
 public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
 
+    @BeforeAll
+    public static void setup(){
+        System.out.println("@BeforeAll - executes once before all test methods in this class.");
+    }
+
+    @BeforeEach
+    public void init(){
+        System.out.println("@BeforeEach - executes before each test method in this class.");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        System.out.println("@AfterEach - executed after each test method.");
+    }
+
+    @AfterAll
+    public static void done(){
+        System.out.println("@AfterAll - executed after all test methods.");
+    }
+
+
     @Test
-//    @DisplayName("Test add(), expected ok")
+    @DisplayName("Test add(), expected ok")
     public void testAddSuccess(){
         var result = calculator.add(5, 10);
         assertEquals(15, result);
     }
 
     @Test
-//    @DisplayName("Test divide(), expected ok")
+    @Disabled
+    @DisplayName("Test divide(), expected ok")
     public void testDivideSuccess(){
         var result = calculator.divide(10, 2);
         assertEquals(5, result);
     }
 
     @Test
-//    @DisplayName("Test method divide(), expected thrown expected thrown exception")
+    @DisplayName("Test method divide(), expected thrown expected thrown exception")
     public void testDivideFailed(){
         assertThrows(IllegalArgumentException.class, () -> {
             calculator.divide(10, 0);
