5640a9f58996792de8bc3e6ae81760a4f01405d1
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2021-02-17 17:31:29 +0700

    concurrent test

--- src/test/java/com/hiwijaya/testing/SlowTest.java
+++ src/test/java/com/hiwijaya/testing/SlowTest.java
@@ -2,18 +2,33 @@ package com.hiwijaya.testing;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author Happy Indra Wijaya
  */
+@Execution(ExecutionMode.CONCURRENT)
 public class SlowTest {
 
     @Test
-    @Timeout(value = 3, unit = TimeUnit.SECONDS)
-    public void testSlow() throws InterruptedException {
-        Thread.sleep(10_000);   // milliseconds
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    public void testSlow1() throws InterruptedException {
+        Thread.sleep(4_000);   // milliseconds
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    public void testSlow2() throws InterruptedException {
+        Thread.sleep(4_000);   // milliseconds
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    public void testSlow3() throws InterruptedException {
+        Thread.sleep(4_000);   // milliseconds
     }
 
 }
